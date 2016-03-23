9b549453e051c86e699f2fa7725a550e2cb032d0
Jasmin Ceco <jasminceco@jasmins-macbook-pro.local>
2016-03-23 12:04:33 +0100

    fist commit

---
+++ registration/Registration.swift
@@ -0,0 +1,34 @@
+//
+//  Registration.swift
+//  registration
+//
+//  Created by Jasmin Ceco on 23/03/16.
+//  Copyright © 2016 Jasmin Ceco. All rights reserved.
+//
+
+import UIKit
+
+
+class Registration {
+    
+    struct personalDetails {
+        static var firsName: String = ""
+        static var lastName: String = ""
+        static var dateOfBirth: String = ""
+        static var gender: String = ""
+    }
+    struct ContactDetails {
+        static var address: String = ""
+        static var zipCode: String = ""
+        static var email: String = ""
+        static var phoneNumber: String = ""
+    }
+    struct securityDetails {
+        static var username: String = ""
+        static var password: String = ""
+        static var confirmPassword: String = ""
+    }
+    
+    
+    
+}
