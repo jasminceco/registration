35c27554ab92253695f0726809d927c4a3254a74
Jasmin Ceco <jasminceco@jasmins-macbook-pro.local>
2016-03-23 11:54:57 +0100

    Initial Commit

---
+++ registration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  registration
+//
+//  Created by Jasmin Ceco on 23/03/16.
+//  Copyright © 2016 Jasmin Ceco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
